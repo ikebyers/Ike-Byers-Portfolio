2694a97dd8f48f5aa592afd6436318dfeb87e7d9
ikebyers <80747699+ikebyers@users.noreply.github.com>
2024-12-13 20:54:15 -0500

    added navbar logo

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -4,51 +4,55 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Nav = () => {
 
-return (
-    <nav 
-        className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-            <NavLink className="navbar-brand" to="/">
-            Ike Byers
-            </NavLink>
-            <button
-                className="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarNavAltMarkup"
-                aria-controls="navbarNavAltMarkup"
-                aria-expanded="false"
-                aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <ul className="nav nav-pills nav-fill">
-                        <li className="nav-item">
-                            <NavLink className="nav-link" exact to="/" activeClassName="active">
-                            About Me
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/portfolio" activeClassName="active">
-                            Portfolio
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/resume" activeClassName="active">
-                            Resume
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/contact" activeClassName="active">
-                            Contact
-                            </NavLink>
-                        </li>
-                    </ul>
+    return (
+        <nav
+            className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <NavLink className="navbar-brand" to="/">
+                        <div className="container">
+                            <a className="navbar-brand" href="#">
+                                <img src="../../../public/ikelogo3.png" alt="ike byers logo"></img>
+                            </a>
+                        </div>
+                </NavLink>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <ul className="nav nav-pills nav-fill">
+                            <li className="nav-item">
+                                <NavLink className="nav-link" exact to="/" activeClassName="active">
+                                    About Me
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/portfolio" activeClassName="active">
+                                    Portfolio
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/resume" activeClassName="active">
+                                    Resume
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/contact" activeClassName="active">
+                                    Contact
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
-    </nav>
+        </nav>
     );
 };
 
